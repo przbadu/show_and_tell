367759ec45c1ad117343223a7aba9107825f4a54
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2020-10-30 20:09:53 +0545

    Add authentication logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { UserProvider } from "./providers/userProvider";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <UserProvider>
+      <Router>
+        {/* <Navbar /> */}
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </UserProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { UserContext } from "../providers/userProvider";
+import { signInWithGoogle } from "../services/firebase";
+
+export default function Login() {
+  const user = useContext(UserContext);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (user) setAuthenticated(true);
+  }, [user]);
+
+  if (authenticated) return <Redirect to="/dashboard" />;
+
+  return (
+    <div className="login-buttons">
+      <button className="login-provider-button" onClick={signInWithGoogle}>
+        <img
+          src="https://img.icons8.com/ios-filled/50/000000/google-logo.png"
+          alt="google icon"
+        />
+        <span> Continue with Google</span>
+      </button>
+    </div>
+  );
+}
